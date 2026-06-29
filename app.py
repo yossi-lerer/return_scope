8ea3934738793c9c659596d820f3387eba25fe00
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-29 11:02:35 +0300

    finish the part two and step 1

--- app.py
+++ app.py
@@ -3,3 +3,16 @@
 # will print 8 and 10
 
 # part 2
+# step 1
+def meter_to_cm(meters):
+    return meters * 100
+
+def msg_distance(msg):
+    return f"Robot moved {msg} centimeters"
+
+def meter_and_msg_distance(meters):
+    cm = meter_to_cm(meters)
+    msg = msg_distance(cm)
+    print(msg)
+
+meter_and_msg_distance(5)
