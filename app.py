a06dacd4585ecd22b127fd745b0e24ab1730cc91
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-29 11:33:10 +0300

    finish the part two and step 2 - Simple Price Calculator

--- app.py
+++ app.py
@@ -4,10 +4,10 @@
 
 # part 2
 # step 1
-def meter_to_cm(meters):
+def meter_to_cm(meters) -> int:
     return meters * 100
 
-def msg_distance(msg):
+def msg_distance(msg) -> str:
     return f"Robot moved {msg} centimeters"
 
 def meter_and_msg_distance(meters):
@@ -16,3 +16,17 @@ def meter_and_msg_distance(meters):
     print(msg)
 
 meter_and_msg_distance(5)
+# step 2 - Simple Price Calculator
+
+def price_with_delivery(price):
+    return price + 10
+
+def multiple_price_by_two(price):
+    return price * 2
+
+def final_price(orginal_price):
+    add_price_delivery = price_with_delivery(orginal_price)
+    Doubling_the_price = multiple_price_by_two(add_price_delivery)
+    print(Doubling_the_price)
+
+final_price(5)
